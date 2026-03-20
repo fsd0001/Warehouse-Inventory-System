60e8096faf8af371a930c58cd0407627fc21a3c0
Muhammad Ahmad <fsddeveloper0001@gmail.com>
2026-03-20 10:19:13 +0500

    Frontend changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import ProductForm from "./components/ProductForm";
+import ProductList from "./components/ProductList";
+import WarehouseForm from "./components/WarehouseForm";
+import WarehouseList from "./components/WarehouseList";
+import InventoryForm from "./components/InventoryForm";
+import InventoryList from "./components/InventoryList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const refresh = () => window.location.reload();
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <div className="container mt-4">
+
+                <div className="row">
+                    <div className="col-md-4">
+                        <ProductForm selectedProduct={selectedProduct} refresh={refresh}/>
+                    </div>
+
+                    <div className="col-md-8">
+                        <ProductList setSelectedProduct={setSelectedProduct}/>
+                    </div>
+                </div>
+
+                <WarehouseForm refresh={refresh}/>
+                <WarehouseList />
+
+                <InventoryForm refresh={refresh}/>
+                <InventoryList />
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
